ac1c68cd31af27066eeae3525bd04bc2b8170b6b
JackTan25 <jacktby@gmail.com>
2026-09-07 14:27:30 +0000

    chore: update data-config.js with repository info

--- js/data-config.js
+++ js/data-config.js
@@ -12,13 +12,13 @@ const DATA_CONFIG = {
      * GitHub repository owner (username)
      * This will be replaced during GitHub Actions workflow execution
      */
-    repoOwner: 'PLACEHOLDER_REPO_OWNER',
+    repoOwner: 'JackTan25',
 
     /**
      * GitHub repository name
      * This will be replaced during GitHub Actions workflow execution
      */
-    repoName: 'PLACEHOLDER_REPO_NAME',
+    repoName: 'ai-infra-daily',
 
     /**
      * Data branch name
